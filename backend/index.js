45bb5a28ef8f6b9e2c820989fe08ab14119ab56c
Kali-Venum <subhamdas.abcd@gmail.com>
2024-10-05 21:36:48 +0530

    changes.

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import mysql from "mysql";
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "node-mysql-crud",
+});
+
+app.get("/", (req, res) => {
+  res.json("hello this is the backend.");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books ('title', 'description', 'cover') VALUES (?)";
+  const values = [
+    "title from backend",
+    "description from backend",
+    "cover from backend",
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+const PORT = 8800;
+app.listen(PORT, () => {
+  console.log("The app is up and running..!");
+});
